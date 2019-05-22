98bd03bc2db97292723287ad70cbed05feedece3
buckingham99 <buckingham99@gmail.com>
2019-05-21 21:34:10 -0700

    Revamped LINQ queries

--- Models/User.cs
+++ Models/User.cs
@@ -53,51 +53,5 @@ namespace BlazorTest.Models
         public double Latitude { get; set; }
         public double Longitude { get; set; }
     }
-    //public class User
-    //{
-    //    public int Number { get; set; }
-    //    public string Gender { get; set; }
-    //    public string NameSet { get; set; }
-    //    public string Title { get; set; }
-    //    public string GivenName { get; set; }
-    //    public string MiddleInitial { get; set; }
-    //    public string Surname { get; set; }
-    //    public string StreetAddress { get; set; }
-    //    public string City { get; set; }
-    //    public string State { get; set; }
-    //    public string StateFull { get; set; }
-    //    public int ZipCode { get; set; }
-    //    public string Country { get; set; }
-    //    public string CountryFull { get; set; }
-    //    public string EmailAddress { get; set; }
-    //    public string Username { get; set; }
-    //    public string Password { get; set; }
-    //    public string TelephoneNumber { get; set; }
-    //    public int TelephoneCountryCode { get; set; }
-    //    public string MothersMaiden { get; set; }
-    //    public string Birthday { get; set; }
-    //    public int Age { get; set; }
-    //    public string TropicalZodiac { get; set; }
-    //    public string CCType { get; set; }
-    //    public long CCNumber { get; set; }
-    //    public int CVV2 { get; set; }
-    //    public string CCExpires { get; set; }
-    //    public string NationalID { get; set; }
-    //    public string UPS { get; set; }
-    //    public Int64 WesternUnionMTCN { get; set; }
-    //    public int MoneyGramMTCN { get; set; }
-    //    public string Color { get; set; }
-    //    public string Occupation { get; set; }
-    //    public string Company { get; set; }
-    //    public string Vehicle { get; set; }
-    //    public string Domain { get; set; }
-    //    public string BloodType { get; set; }
-    //    public double Pounds { get; set; }
-    //    public string Kilograms { get; set; }
-    //    public string FeetInches { get; set; }
-    //    public double Centimeters { get; set; }
-    //    public string GUID { get; set; }
-    //    public double Latitude { get; set; }
-    //    public double Longitude { get; set; }
-    //}
+
 }
